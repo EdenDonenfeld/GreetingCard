d414ef727fc53327e0c255ee0352b15d452bbc3d
EdenDonenfeld <edendonenfeld@gmail.com>
2025-06-19 12:31:37 +0300

    reset 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import GreetingCard from './components/GreetingCard';
 
 function App() {
   return (
-    <GreetingCard name="Zoey" message="Welcome to course Kerem 6!" />
+    <p>Hello World</p>
   );
 }
 
